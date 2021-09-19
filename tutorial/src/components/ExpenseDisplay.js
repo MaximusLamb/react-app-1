8d8ec3e89f14f9af9e0fbfc8f5131f4cafac4fa7
Max Lamb <maxlambcodes@gmail.com>
2021-09-19 08:26:03 -0700

    little more work

--- tutorial/src/components/ExpenseDisplay.js
+++ tutorial/src/components/ExpenseDisplay.js
@@ -1,14 +1,14 @@
 import './ExpenseDisplay.css';
 import { ExpenseItem } from './ExpenseItem';
+import Card from './Card'
 
 export function ExpenseDisplay(props) {
 
+const expense = props.expenses;
+
   return (
-    <div className="expenses">
-      <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
-      <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-      <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
-      <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>
-    </div>
+    <Card className="expenses">
+      {expense.map(e=>(<ExpenseItem title={e.title} amount={e.amount} date={e.date}/>))}
+    </Card>
   )
 }
--- tutorial/src/components/ExpenseItem.js
+++ tutorial/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import { ExpenseDate } from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from './Card';
 
 export function ExpenseItem(props) {
   
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
      <ExpenseDate date={props.date}/>
       <h2 className="expense-item__description">{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
-    </div>
+    </Card>
   )
 }
 
